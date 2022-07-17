a97e939d027644bb8ddb6d00d555d26786b963d3
Ronald Javier Albor Cardenas <ralbor@nitrofert.com.co>
2022-07-17 18:39:29 -0500

    Modificar el app component htm y reubicar el sidebar dentro del sidebar component html

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+
 
 
 
@@ -10,7 +13,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
   exports:[SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
