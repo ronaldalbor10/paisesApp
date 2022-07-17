817a9d2b7d94d867f1d0cae3945de462a766841e
Ronald Javier Albor Cardenas <ralbor@nitrofert.com.co>
2022-07-17 16:57:45 -0500

    Generacion de modulos y componentes, exportación de componentes y modulos para ser utilizados en el proyecto

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html'
+})
+export class PorPaisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
